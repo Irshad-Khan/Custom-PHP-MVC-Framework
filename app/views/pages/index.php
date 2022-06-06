e057666dc7ea51993573e884ddfb4369373d0ac8
IrshadKhan1314 <m.irshad@vfairs.com>
2022-06-06 21:25:19 +0500

    text added in index page

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,3 +1,4 @@
 <?php require APP_ROOT.'/views/inc/header.php'; ?>
-<h3>Index</h3>
+<h3>Khan Framework</h3>
+<p>This Khan Framework of php. You have to designed PHP Application easily with it.</p>
 <?php require APP_ROOT.'/views/inc/footer.php'; ?>
